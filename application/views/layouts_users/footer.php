d872fe1b5930d9c094d16c5f3e3d4cdb5275ac7b
KangSDhi <kangteknisi@gmail.com>
2023-01-26 14:43:26 +0700

    Membuat Layout Member

---
+++ application/views/layouts_users/footer.php
@@ -0,0 +1,28 @@
+<footer class="footer-area">
+	<div class="container">
+		<div class="row footer-bottom d-flex justify-content-between align-items-center">
+			<p class="col-lg col-md footer-text text-center">
+				Copyright &copy; 
+				<script>
+					document.write(new Date().getFullYear());
+				</script>
+				By KangSDhi. All rights reserved. V 1.0.0
+			</p>
+		</div>
+	</div>
+</footer>
+
+<script src="<?php echo base_url();?>assets/bulkapp/js/jquery-3.2.1.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/popper.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/stellar.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/lightbox/simpleLightbox.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/isotope/isotope-min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/jquery.ajaxchimp.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/counter-up/jquery.counterup.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/mail-script.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/vendors/popup/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url();?>assets/bulkapp/js/theme.js"></script>
+<script src="<?php echo base_url();?>assets/sweetalert2/dist/sweetalert2.all.min.js"></script>
---
+++ application/views/layouts_users/head.php
@@ -0,0 +1,17 @@
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="<?php echo base_url(); ?>assets/bulkapp/img/favicon.ico" type="image/png">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/linericon/style.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/lightbox/simpleLightbox.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/nice-select/css/nice-select.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/animate-css/animate.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/vendors/popup/magnific-popup.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bulkapp/css/responsive.css">
+</head>
