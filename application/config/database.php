a6cd57728cac616d246750c3dfa22be4b7fc7ce3
KangSDhi <kangteknisi@gmail.com>
2023-01-26 10:38:51 +0700

    Mengatur File database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'database',
+	'username' => 'ci',
+	'password' => 'ci',
+	'database' => 'simulasi-db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
