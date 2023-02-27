b8769cfc05335460e027dddd590f754eed60d700
KangSDhi <kangteknisi@gmail.com>
2023-02-27 15:15:08 +0700

    Membuat Halaman Bank Soal - To Be Continued

--- application/views/v_bankSoal/index.php
+++ application/views/v_bankSoal/index.php
@@ -124,6 +124,156 @@
                 ]
             });
 
+            // Save input data bank soal
+            $('#btn-saveBankSoal').on('click', function(){
+                const idKategoriSoal = $('#addBankSoal').attr('value');
+                const form = $('#formInputBankSoal');
+
+                $.ajax({
+                    url: "<?php echo base_url('banksoal/add/') ?>" + idKategoriSoal;
+                    method: "POST",
+                    data: form.serialize(),
+                    dataType: "JSON",
+                    success: function(data){
+                        // Data Error
+                        if (data.error) {
+
+                            if(data.pertanyaan_error != '')
+                            $('#pertanyaan_error').html('data.pertanyaan_error');
+                            else
+                            $('#pertanyaan_error').html('');
+
+                            if(data.pilihan_a_error != '')
+                            $('#pilihan_a_error').html('data.pilihan_a_error');
+                            else
+                            $('#pilihan_a_error').html('');
+
+                            if(data.pilihan_b_error != '')
+                            $('#pilihan_b_error').html('data.pilihan_b_error');
+                            else
+                            $('#pilihan_b_error').html('');
+
+                            if(data.pilihan_c_error != '')
+                            $('#pilihan_c_error').html('data.pilihan_c_error');
+                            else
+                            $('#pilihan_c_error').html('');
+
+                            if(data.pilihan_d_error != '')
+                            $('#pilihan_d_error').html('data.pilihan_d_error');
+                            else
+                            $('#pilihan_d_error').html('');
+
+                            if(data.pilihan_e_error != '')
+                            $('#pilihan_e_error').html('data.pilihan_e_error');
+                            else
+                            $('#pilihan_e_error').html('');
+
+                            if(data.nilai_a_error != '')
+                            $('#nilai_a_error').html('data.nilai_a_error');
+                            else
+                            $('#nilai_a_error').html('');
+
+                            if(data.nilai_b_error != '')
+                            $('#nilai_b_error').html('data.nilai_b_error');
+                            else
+                            $('#nilai_b_error').html('');
+
+                            if(data.nilai_c_error != '')
+                            $('#nilai_c_error').html('data.nilai_c_error');
+                            else
+                            $('#nilai_c_error').html('');
+
+                            if(data.nilai_d_error != '')
+                            $('#nilai_d_error').html('data.nilai_d_error');
+                            else
+                            $('#nilai_d_error').html('');
+
+                            if(data.nilai_e_error != '')
+                            $('#nilai_e_error').html('data.nilai_e_error');
+                            else
+                            $('#nilai_e_error').html('');
+                        }
+
+                        // Data Bank Soal Berhasil Disimpan
+                        if (data.success) {
+                            form.trigger('reset');
+                            $('#modalAdd').modal('hide');
+                            // $('.textarea').next().find(".note-editable").text("");
+                            $('.textarea').summernote('code', '');
+                            $('#pertanyaan_error').html('');
+                            $('#pilihan_a_error').html('');
+                            $('#pilihan_b_error').html('');
+                            $('#pilihan_c_error').html('');
+                            $('#pilihan_d_error').html('');
+                            $('#pilihan_e_error').html('');
+                            $('#nilai_a_error').html('');
+                            $('#nilai_b_error').html('');
+                            $('#nilai_c_error').html('');
+                            $('#nilai_d_error').html('');
+                            $('#nilai_e_error').html('');
+                            $('#example').DataTable().ajax.reload();
+                            toastr.success('Data bank soal berhasil disimpan.');
+                        }
+                    }
+                });
+            });
+
+            // Hapus data bank soal
+            $('body').on('click', '.btn-delete', function(e){
+                e.preventDefault();
+
+                // Alamat Controller Delete Bank Soal
+                const url = $(this).attr('href');
+
+                Swal.fire({
+                    title: 'Hapus Data',
+                    text: "Anda Ingin Menghapus Data?",
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.value) {
+                        $.ajax({
+                            url: url,
+                            method: "POST",
+                            success: function(response){
+                                $('#example').DataTable().ajax.reload();
+                                toastr.error('Data bank soal berhasil dihapus.');
+                            }
+                        });
+                    }
+                });
+            });
+
+            // Tampil modal edit data bank soal
+            $('body').on('click', '.btn-edit', function(e) {
+                e.preventDefault();
+                const idBankSoal = $(this).attr('value');
+
+                $.ajax({
+                    url: "<?php echo base_url('banksoal/ajaxUpdate/') ?>" + idBankSoal,
+                    type: "GET",
+                    dataType: "JSON",
+                    success: function(data){
+                        $('.textarea2').summernote('code', data.pertanyaan);
+                        $('[name="idKategoriSoal"]').val(data.kategori_id);
+                        $('[name="idBankSoal"]').val(data.id);
+                        $('[name="pilihan_a2"]').val(data.pilihan_a);
+                        $('[name="pilihan_b2"]').val(data.pilihan_b);
+                        $('[name="pilihan_c2"]').val(data.pilihan_c);
+                        $('[name="pilihan_d2"]').val(data.pilihan_d);
+                        $('[name="pilihan_e2"]').val(data.pilihan_e);
+                        $('[name="nilai_a2"]').val(data.nilai_a);
+                        $('[name="nilai_b2"]').val(data.nilai_b);
+                        $('[name="nilai_c2"]').val(data.nilai_c);
+                        $('[name="nilai_d2"]').val(data.nilai_d);
+                        $('[name="nilai_e2"]').val(data.nilai_e);
+                        $('#modal-edit').modal('show');
+                    }
+                });
+            });
+
             
         });
     </script>
